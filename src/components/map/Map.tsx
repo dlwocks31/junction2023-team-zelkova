4d14358b39423b8e20d2ff4caf367eadb21bf5d3
jyp930 <jyp930@gmail.com>
2023-08-19 06:04:51 +0900

    test service worker notification

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -87,24 +87,34 @@ const Map = ({
 
   /** notification **/
   function notifyMe() {
-    if (!("Notification" in window)) {
-      // Check if the browser supports notifications
-      alert("This browser does not support desktop notification");
-    } else if (Notification.permission === "granted") {
-      // Check whether notification permissions have already been granted;
-      // if so, create a notification
-      const notification = new Notification("Hi there!");
-      // …
-    } else if (Notification.permission !== "denied") {
-      // We need to ask the user for permission
-      Notification.requestPermission().then((permission) => {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          const notification = new Notification("Hi there!");
-          // …
-        }
-      });
-    }
+    navigator.serviceWorker.register("sw.js").then(() => {
+      if (!("Notification" in window)) {
+        // Check if the browser supports notifications
+        alert("This browser does not support desktop notification");
+      } else if (Notification.permission === "granted") {
+        // Check whether notification permissions have already been granted;
+        // if so, create a notification
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.showNotification("Hi Junction!", {
+            body: "Buzz! Buzz!",
+            vibrate: [200, 100, 200],
+          });
+        });
+      } else if (Notification.permission !== "denied") {
+        // We need to ask the user for permission
+        Notification.requestPermission().then((permission) => {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
+            navigator.serviceWorker.ready.then((registration) => {
+              registration.showNotification("Hi Junction!", {
+                body: "Buzz! Buzz!",
+                vibrate: [200, 100, 200],
+              });
+            });
+          }
+        });
+      }
+    });
   }
 
   return (
