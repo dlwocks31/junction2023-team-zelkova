71113fdcc62701f84a90842a8976a5af98273220
jyp930 <jyp930@gmail.com>
2023-08-19 10:00:43 +0900

    remove unusable code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,9 @@
 import { NextSeo } from "next-seo";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import { useEffect } from "react";
-import { initializeApp } from "firebase/app";
-import { getMessaging, onMessage, getToken } from "firebase/messaging";
 
 export default function Home() {
   const hello = api.findAllRestaurant.useQuery({});
-
-  // FCM
-  const onMessageFCM = async () => {
-    // 브라우저에 알림 권한을 요청합니다.
-    const permission = await Notification.requestPermission();
-    if (permission !== "granted") return;
-
-    const firebaseApp = initializeApp({
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    });
-
-    const messaging = getMessaging(firebaseApp);
-
-    getToken(messaging, {
-      vapidKey: process.env.NEXT_PUBLIC_FCM_KEY,
-    })
-      .then((currentToken) => {
-        if (currentToken) {
-          console.log(currentToken);
-        } else {
-          console.log(
-            "No registration token available. Request permission to generate one."
-          );
-        }
-      })
-      .catch((err) => {
-        console.log("An error occurred while retrieving token. ", err);
-      });
-
-    // 메세지가 수신되면 역시 콘솔에 출력합니다.
-    onMessage(messaging, (payload) => {
-      console.log("Message received. ", payload);
-    });
-  };
-
-  useEffect(() => {
-    onMessageFCM();
-  }, []);
 
   return (
     <>
