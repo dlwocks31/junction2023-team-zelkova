a786c183f009bce9ea474b1d03b30ac9a039da94
jyp930 <jyp930@gmail.com>
2023-08-19 13:28:02 +0900

    fix console error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,34 +2,69 @@ import { NextSeo } from "next-seo";
 import { api } from "~/utils/api";
 import dayjs from "dayjs";
 import { IconButton, NearMeIcon } from "loplat-ui";
+import { useState } from "react";
 
 export default function Home() {
   const hello = api.findAllRestaurant.useQuery({});
+  const [messages, setMessages] = useState<
+    {
+      speaker: "bot" | "human";
+      content: string;
+    }[]
+  >([]);
+
+  const [currentMessage, setCurrentMessage] = useState("");
+  const sendMessage = () => {
+    setMessages((state) => [
+      ...state,
+      { speaker: "human", content: currentMessage },
+    ]);
+    setCurrentMessage("");
+  };
 
   return (
     <>
       <NextSeo title="채팅" description="Bob과 대화를 나누어보아요." />
       {/*<pre>{JSON.stringify(hello.data)}</pre>*/}
       <main style={{ width: "100%", height: "100%" }}>
-        <p className="time">{dayjs().format("YYYY.MM.DD HH:mm a")}</p>
-        <div className="intro">
-          <div className="rabbit">
-            <div className="speech">
-              Do you want something to eat now? Let&apos;s go get it together 😋
+        {messages.length === 0 && (
+          <div className="intro">
+            <div className="rabbit">
+              <div className="speech">
+                Do you want something to eat now? Let&apos;s go get it together
+                😋
+              </div>
+              <img
+                src="/gif/walking.gif"
+                alt=""
+                style={{ width: "50%", margin: "auto" }}
+              />
             </div>
-            <img
-              src="/gif/walking.gif"
-              alt=""
-              style={{ width: "50%", margin: "auto" }}
-            />
           </div>
-        </div>
-        <div className="sendMessage">
-          <input className="input" />
-          <IconButton variant="ghost2" borderless style={{ padding: 8 }}>
-            <NearMeIcon fill="#000000" size={20} suffixForId="" />
+        )}
+        {messages.length > 0 && (
+          <div>
+            <p className="time">{dayjs().format("YYYY.MM.DD HH:mm a")}</p>
+          </div>
+        )}
+        <form
+          className="sendMessage"
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendMessage();
+          }}
+        >
+          <input
+            className="input"
+            value={currentMessage}
+            onChange={(e) => {
+              setCurrentMessage(e.target.value);
+            }}
+          />
+          <IconButton variant="ghost2" borderless>
+            <NearMeIcon fill="#000000" size={20} suffixForId="nearMe" />
           </IconButton>
-        </div>
+        </form>
       </main>
       <style jsx>
         {`
@@ -41,7 +76,7 @@ export default function Home() {
           }
           .intro {
             position: relative;
-            margin-top: 96px;
+            margin-top: 120px;
             width: 100%;
 
             .rabbit {
