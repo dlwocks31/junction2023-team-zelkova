1d2a6c6ebf7a3e8417d42d9bd9a7e5c70a949c76
HHCHU <ded06031@snu.ac.kr>
2023-08-20 00:43:02 +0900

    Mypage added

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,26 @@
+import { NextSeo } from "next-seo";
+import { useState } from "react";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+export default function Mypage() {
+  const router = useRouter();
+  const [paymentImageState, setPaymentImageState] = useState("initial");
+
+  return (
+    <>
+      <NextSeo title="내 정보" description="TODO" />
+      <main
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden",
+        }}
+      >
+        <img src="/image/myPageView.png" alt="" style={{ width: "100%" }} />
+      </main>
+      <style jsx>{``}</style>
+    </>
+  );
+}
